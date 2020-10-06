d6f00288983f7fca44d2a656e6f1fe3672df0264
phpilatso <phenyopilatso@momentum.co.za>
2020-10-06 14:03:36 +0200

    Add MapStruct for Type Conversion. closes #9

---
+++ target/generated-sources/annotations/guru/springframework/beerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,64 @@
+package guru.springframework.beerservice.web.mappers;
+
+import guru.springframework.beerservice.domain.Beer;
+import guru.springframework.beerservice.domain.Beer.BeerBuilder;
+import guru.springframework.beerservice.web.model.BeerDto;
+import guru.springframework.beerservice.web.model.BeerDto.BeerDtoBuilder;
+import guru.springframework.beerservice.web.model.BeerStyleEnum;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-10-06T14:01:48+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Amazon.com Inc.)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.id( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDto.version( beer.getVersion().intValue() );
+        }
+        beerDto.beerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.beerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
+        }
+        if ( beer.getUpc() != null ) {
+            beerDto.upc( beer.getUpc() );
+        }
+        beerDto.price( beer.getPrice() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( beerDto.getId() );
+        if ( beerDto.getVersion() != null ) {
+            beer.version( beerDto.getVersion().longValue() );
+        }
+        beer.beerName( beerDto.getBeerName() );
+        if ( beerDto.getBeerStyle() != null ) {
+            beer.beerStyle( beerDto.getBeerStyle().name() );
+        }
+        beer.upc( beerDto.getUpc() );
+        beer.price( beerDto.getPrice() );
+
+        return beer.build();
+    }
+}
