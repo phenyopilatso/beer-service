0bb38b05897b52134fbd1ede66243b91fb8c96c1
phpilatso <phenyopilatso@momentum.co.za>
2020-10-20 12:52:47 +0200

    Fix Hibernate Issues on Startup with MySql closes #26

--- src/main/java/guru/springframework/beerservice/BeerServiceApplication.java
+++ src/main/java/guru/springframework/beerservice/BeerServiceApplication.java
@@ -2,8 +2,9 @@ package guru.springframework.beerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerServiceApplication {
 
     public static void main(String[] args) {
